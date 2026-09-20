68cdd31331876f018c687c3433e0177346122008
josephherlihy77 <josephherlihy77@gmail.com>
2026-09-20 02:03:01 -0400

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='qfl-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./data/snapshot.json']))));self.addEventListener('fetch',e=>{if(e.request.url.includes('/data/latest.json'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
